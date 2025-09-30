f1d9212b45f573ea56d2ef209f698f8e4318092c
yeekong <yeekong.info@gmail.com>
2025-09-30 12:23:27 +0800

    -

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -95,12 +95,12 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_LPTIM1_Init();
-  MX_I2C1_Init();
-  MX_I2C2_Init();
-  MX_LPUART1_UART_Init();
-  MX_USART1_UART_Init();
-  MX_SPI1_Init();
+MX_LPTIM1_Init();
+MX_I2C1_Init();
+MX_I2C2_Init();
+MX_LPUART1_UART_Init();
+MX_USART1_UART_Init();
+MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
   BatteryMonitor_Init();
 
